708d6a2f913e2769f61409774d7515d681008d06
Luis Palma <palmaluis1997@gmail.com>
2021-03-12 10:47:35 -0400

    url

--- src/boot/services.js
+++ src/boot/services.js
@@ -3,7 +3,7 @@ import services from '../../microservices/services'
 import mockData from '../../mockData/services'
 
 Vue.use(services, {
-  baseUrl: 'http://192.168.10.45:8080/api.rest/api'
+  baseUrl: 'http://127.0.0.1:8080/api.rest/api'
   // baseUrl: 'http://192.168.10.45:8080/api.rest/api'
 })
 
