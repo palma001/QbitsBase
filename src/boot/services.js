66b8b2cfdca5e7c4a36bad423b8737736971eee2
Luis Palma <palmaluis1997@gmail.com>
2021-04-10 17:37:07 -0400

    url

--- src/boot/services.js
+++ src/boot/services.js
@@ -3,8 +3,8 @@ import services from '../../microservices/services'
 import mockData from '../../mockData/services'
 
 Vue.use(services, {
-  baseUrl: 'http://127.0.0.1:8000/api/'
-  // baseUrl: 'http://192.168.10.45:8080/api.rest/api'
+  // baseUrl: 'http://127.0.0.1:8000/api/'
+  baseUrl: 'http://api-transporte.herokuapp.com/api'
 })
 
 Vue.use(mockData, {
