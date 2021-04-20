a214220eea5b307d40981b223ebe96a47a8ffd78
Luis Palma <palmaluis1997@gmail.com>
2021-04-19 20:41:27 -0400

    change

---
+++ src/pages/ReceptionPakagesPage.vue
@@ -0,0 +1,120 @@
+<template>
+  <q-page padding>
+    <div class="row q-col-gutter-md">
+      <div class="col-lg-2 col-md-2 col-sm-3 col-xs-12">
+        <q-input
+        label="Código de guia"
+        dense
+        autofocus
+        v-model="codeGuide"
+        @keyup.native.enter="getOneGuide"
+        />
+      </div>
+      <div class="col-lg-2 col-md-2 col-md-2 col-sm-2 col-xs-12" v-if="guide.carrier">
+        <q-input
+          label="Transportista"
+          dense
+          v-model="guide.carrier.full_name"
+          readonly
+        />
+      </div>
+      <div class="col-lg-2 col-md-2 col-md-2 col-sm-2 col-xs-12" v-if="guide.vehicle">
+        <q-input
+          label="Marca"
+          dense
+          v-model="guide.vehicle.brand"
+          readonly
+          hint="Datos del vehiculo"
+        />
+      </div>
+      <div class="col-lg-2 col-md-2 col-md-2 col-sm-2 col-xs-12" v-if="guide.vehicle">
+        <q-input
+          label="Placa"
+          dense
+          v-model="guide.vehicle.plate"
+          readonly
+          hint="Datos del vehiculo"
+        />
+      </div>
+      <div v-if="guide.seals" class="col-12 row q-col-gutter-md">
+        <div class="col-3" v-for="(seal, index) in guide.seals" :key="seal.id">
+          <q-input
+            :label="`Precinto Nro ${index + 1}`"
+            v-model="seal.seal_number"
+            dense
+            readonly
+          />
+        </div>
+      </div>
+      <div class="col-12">
+        <data-table
+          title="list"
+          module="voucher"
+          toggable
+          :column="voucherConfig"
+          :data="guide.vouchers"
+          :loading="loadingTable"
+          :optionPagination="optionPagination"
+          @delete="deleteVocuher"
+        />
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import DataTable from '../components/DataTable.vue'
+import { voucherConfig } from '../config-file/voucher/voucherConfig'
+import { mixins } from '../mixins'
+export default {
+  mixins: [mixins.containerMixin],
+  components: {
+    DataTable
+  },
+  data () {
+    return {
+      voucherConfig,
+      codeGuide: null,
+      guide: {
+        vouchers: []
+      },
+      voucherSelected: [],
+      loadingTable: false,
+      /**
+       * Options pagination
+       * @type {Object}
+       */
+      optionPagination: {
+        rowsPerPage: 20,
+        paginate: true,
+        sortBy: 'id',
+        sortOrder: 'desc'
+      }
+    }
+  },
+  methods: {
+    deleteVocuher (data) {
+      console.log(data)
+    },
+    /**
+     * Get one voucher
+     */
+    getOneGuide (code) {
+      this.codeGuide = typeof code === 'string' ? code : this.codeGuide
+      this.$services.getOneData(['guides', this.codeGuide])
+        .then(({ res }) => {
+          if (res.data) {
+            this.guide = res.data
+          }
+        })
+        .catch(() => {
+          this.notify(this, 'Guia no contrada', 'negative', 'warning')
+        })
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
